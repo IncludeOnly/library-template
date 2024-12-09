db4a71adbb1a388b3af091e40a17f96a80375366
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-12-09 23:14:50 +0200

    added files

---
+++ library.h
@@ -0,0 +1,16 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+
+#define LIBRARY_VERSION_MAJOR 0
+#define LIBRARY_VERSION_MINOR 0
+#define LIBRARY_VERSION_PATCH 1
+#define LIBRARY_VERSION "0.0.1"
+
+#ifndef LIBAPI
+    #define LIBAPI static
+#endif // LIBAPI
+
+
+#endif // LIBRARY_H
+
